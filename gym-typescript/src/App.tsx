482446f616b9a4931b39edd1d85c93e25952df61
wanessa_laurindo <wanessawlrodrigues@gmail.com>
2024-12-18 10:39:29 -0300

    feat: navbar menu

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
+
+
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
   return (
-    <div className="app">app</div>
+    <div className="app bg-gray-20">
+      <Navbar 
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
+    </div>
   )
 }
 
-export default App
+export default App;
